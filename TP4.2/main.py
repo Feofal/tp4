0de018b165d839cf89c7ebfc562c0c3a563f2751
Feofal <forielfusierg@educ.college-montreal.qc.ca>
2024-12-19 21:07:47 -0500

    Update main.py

--- TP4.2/main.py
+++ TP4.2/main.py
@@ -8,12 +8,14 @@ from random import randint
 def dice_roll(num_dice, num_face):
     result = randint(num_dice, num_face * num_dice)
     return result
+
 def choose_dice_roll():
     dice_roll_list = []
     for i in range(4):
         dice_roll_list.append(dice_roll(1,6))
     dice_roll_list.remove(min(dice_roll_list))
     return sum(dice_roll_list)
+
 class NPC:
     def __init__(self, name, hp, race, species, classes):
         self.strenght = choose_dice_roll()
@@ -30,12 +32,13 @@ class NPC:
         self.classes = classes
     def show_stat(self):
         print(f"Nom: {self.name}\nRace: {self.race}\nEspèce: {self.species}\nClasse: {self.classes}\nPv: {self.hp}\nArmure: {self.armor}\nForce: {self.strenght}\nAgilité: {self.dexterity}\nConstitution: {self.constitution}\nIntelligence: {self.intelligence}\nSagesse: {self.wisdom}\nCharisme: {self.charisma}\n")
+
 class Kobold(NPC):
     def __init__(self):
         super().__init__("Kabald", dice_roll(1, 20),"Kobold", "Kobold", "Combattant")
+
     def attack(self, target):
         self.target = target
-
         self.try_attack = dice_roll(1,20)
         match self.try_attack:
             case 20:
@@ -50,13 +53,13 @@ class Kobold(NPC):
             case _ if self.try_attack >= 2 and self.try_attack <= 19 and self.try_attack <= self.target.armor:
                 self.damage = 0
                 print(f"L'attaque est ratée.")
-
         return self.damage
 
     def receive_damage(self, opponent_damage):
         self.opponent_damage = opponent_damage
         self.hp -= self.opponent_damage
         print(f"Il reste {self.hp} pv a {self.name}.")
+
 class Hero(NPC):
     def __init__(self):
         super().__init__("Sir Otho Pouléto", dice_roll(1, 20), "Gallus", "Bright Gallus", "Bard")
@@ -77,7 +80,6 @@ class Hero(NPC):
             case _ if self.try_attack >= 2 and self.try_attack <= 19 and self.try_attack <= self.target.armor:
                 self.damage = 0
                 print(f"L'attaque est ratée.")
-
         return self.damage
 
     def receive_damage(self, opponent_damage):
@@ -91,15 +93,3 @@ h.show_stat()
 k.show_stat()
 h.receive_damage(k.attack(h))
 k.receive_damage(h.attack(k))
-
-
-
-
-
-
-
-
-
-
-
-
